5004433dd01471e322c6e3f8b6e4d8603b9c20dc
Shyam Babu Jayswal <jaisnj541@gmail.com>
2022-08-24 22:14:09 +0530

    Initial commit

---
+++ Usingarraylist.java
@@ -0,0 +1,28 @@
+//list.get()   ---- return value of that index
+//list.size-1 -------last index of list
+public class Usingarraylist {
+    static class stack {
+        ArrayList<Integer> List = new ArrayList<>();
+    if(list.size==0)
+
+        {
+            return;
+        }
+
+        //push
+        public static void push(int data) {
+            list.add(data);
+        }
+
+        //pop
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = list.get(list.size - 1);
+            list.reomve(list.size - 1);
+            return top;
+        }
+
+    }}
+
